70d4f8ab672501ff3062c1cdde545dcbe6c538e4
Marouf <maroufibrahimi7264@gmail.com>
2023-02-25 15:33:34 -0800

    added link of bootstrap-icons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./components/App";
-import './index.css'
+import "./index.css";
+import "./assets/bootstrap-icons/bootstrap-icons.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
