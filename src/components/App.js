dffd9d43af124066be7b695c4ed01e5a40c09c5a
Marouf <maroufibrahimi7264@gmail.com>
2023-02-27 14:39:10 -0800

    addContactHandler method created

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import AddContact from "./AddContact/AddContact";
 import ContactList from "./ContactList/ContactList";
 
-import './App.css'
+import "./App.css";
 
 const App = () => {
+  const LOCAL_STROAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, { id: uuidv4(), ...contact }]);
+  };
+
   return (
     <div className="container">
       <div>
-        <AddContact />
+        <AddContact addContactHandler={addContactHandler} />
       </div>
 
       <div>
-        <ContactList />
+        <ContactList contacts={contacts} />
       </div>
     </div>
   );
