3a76edaf4372f66132742871fb58f460818e0c4f
Marouf <maroufibrahimi7264@gmail.com>
2023-02-26 19:55:15 -0800

    struture for contact list

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { useState } from "react";
 
 import "./ContactList.css";
 
-const ContactLIst = () => {
-  return <div>ContactLIst</div>;
+const ContactList = () => {
+  const time = new Date().toLocaleTimeString();
+  const [upTime, setUpTime] = useState(time);
+
+  setInterval((time) => {
+    time = new Date().toLocaleTimeString();
+    setUpTime(time);
+  }, 1000);
+
+  return (
+    <div className="contact_list">
+      <div className="notification_area">
+        <div className="notification_area_left">
+          <i className="bi bi-envelope-fill"></i>
+          <i className="bi bi-telegram"></i>
+          <i className="bi bi-linkedin"></i>
+        </div>
+        <div className="notification_area_right">
+          <i className="bi bi-wifi"></i>
+          <i className="bi bi-battery-half"></i>
+          <i>{upTime}</i>
+        </div>
+      </div>
+
+      {/* render the contact card */}
+      <div className="render_contact_card"></div>
+
+      <div className="footer_area">
+        <div>
+          <i className="bi bi-arrows-angle-contract"></i>
+        </div>
+        <div>
+          <i className="bi bi-crop"></i>
+        </div>
+        <div>
+          <i className="bi bi-arrow-left"></i>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default ContactLIst;
+export default ContactList;
