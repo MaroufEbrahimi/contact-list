03060c82c0a60e74a8cf71e96dbc57bfd6f3d9fc
Marouf <maroufibrahimi7264@gmail.com>
2023-02-26 18:05:09 -0800

    added form validation

--- src/components/AddContact/AddContact.js
+++ src/components/AddContact/AddContact.js
@@ -3,7 +3,27 @@ import React from "react";
 import "./AddContact.css";
 
 const AddContact = () => {
-  return <div>AddContact</div>;
+  return (
+    <div className="login_container">
+      <form>
+        <div className="input_box">
+          <input type="text" required />
+          <span></span>
+          <label>User Name</label>
+        </div>
+
+        <div className="input_box">
+          <input type="text" required />
+          <span></span>
+          <label>Phone Number</label>
+        </div>
+
+        <div className="submit">
+          <input type="submit" value="Add" className="btn" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddContact;
