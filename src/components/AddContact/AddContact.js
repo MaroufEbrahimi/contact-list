e31ac5a4ba79e038bdebd69b38b963ea47d0bb15
Marouf <maroufibrahimi7264@gmail.com>
2023-02-26 18:49:23 -0800

    dynamic value for input box

--- src/components/AddContact/AddContact.js
+++ src/components/AddContact/AddContact.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./AddContact.css";
 
-const AddContact = () => {
+const AddContact = (props) => {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+
   return (
     <div className="login_container">
       <form>
         <div className="input_box">
-          <input type="text" required />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
           <span></span>
           <label>User Name</label>
         </div>
 
         <div className="input_box">
-          <input type="text" required />
+          <input
+            type="tel"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+            required
+          />
           <span></span>
           <label>Phone Number</label>
         </div>
