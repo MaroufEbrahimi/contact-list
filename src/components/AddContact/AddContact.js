f8c6413e78b41b426d27901264f66b7e9c99ee61
Marouf <maroufibrahimi7264@gmail.com>
2023-02-27 14:38:21 -0800

    change function component to class component

--- src/components/AddContact/AddContact.js
+++ src/components/AddContact/AddContact.js
@@ -1,60 +1,73 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 
 import "./AddContact.css";
 
-const AddContact = (props) => {
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
+class AddContact extends Component {
+  state = {
+    name: "",
+    number: "",
+  };
 
-  const add = (e) => {
+  add = (e) => {
     e.preventDefault();
-    if (name === "" || name === " " || number === "" || number === " ") {
+    if (
+      this.state.name === "" ||
+      this.state.name === " " ||
+      this.state.number === "" ||
+      this.state.number === " "
+    ) {
       alert("All the field are mandatory!");
       return;
     }
-    props.addContactHandler(name, number);
-    setName("");
-    setNumber("");
+    this.props.addContactHandler(this.state);
+    this.setState({ name: "", number: "" });
   };
 
-  return (
-    <div className="login_container">
-      <form onSubmit={add}>
-        <div className="input_box">
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-          <span></span>
-          <label>User Name</label>
-        </div>
+  render() {
+    return (
+      <div className="login_container">
+        <form onSubmit={this.add}>
+          <div className="input_box">
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={(e) => this.setState({ name: e.target.value })}
+              required
+            />
+            <span></span>
+            <label>User Name</label>
+          </div>
 
-        <div className="input_box">
-          <input
-            type="tel"
-            value={number}
-            onChange={(e) => setNumber(e.target.value)}
-            required
-          />
-          <span></span>
-          <label>Phone Number</label>
-        </div>
+          <div className="input_box">
+            <input
+              type="tel"
+              value={this.state.number}
+              onChange={(e) => this.setState({ number: e.target.value })}
+              required
+            />
+            <span></span>
+            <label>Phone Number</label>
+          </div>
 
-        <div className="file_handler">
-          <input type="file" name="image-upload" id="input" accept="image/*" />
-          <label htmlFor="input" className="image-upload">
-            <i className="bi bi-image-fill" title="choose your photo"></i>
-          </label>
-        </div>
+          <div className="file_handler">
+            <input
+              type="file"
+              name="image-upload"
+              id="input"
+              accept="image/*"
+            />
+            <label htmlFor="input" className="image-upload">
+              <i className="bi bi-image-fill" title="choose your photo"></i>
+            </label>
+          </div>
 
-        <div className="submit">
-          <input type="submit" value="Add" className="btn" />
-        </div>
-      </form>
-    </div>
-  );
-};
+          <div className="submit">
+            <input type="submit" value="Add" className="btn" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default AddContact;
