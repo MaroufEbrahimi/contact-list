d2e6226c5793990e0875d24311313200963d1e57
Marouf <maroufibrahimi7264@gmail.com>
2023-02-26 20:05:28 -0800

    add name and number to contact card

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -1,9 +1,27 @@
 import React from "react";
 
 import "./ContactCard.css";
+import profile from "../../images/profile.jpg";
 
-const ContactCard = () => {
-  return <div>ContactCard</div>;
+const ContactCard = (props) => {
+  return (
+    <div className="contact_card">
+      <div className="contact_card_divs">
+        <div className="contact_card_img">
+          <img src={profile} />
+        </div>
+
+        <div className="contact_card_ditails">
+          <h4>name</h4>
+          <p>0121212</p>
+        </div>
+
+        <div className="contact_card_delete">
+          <i className="bi bi-trash"></i>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactCard;
