ad8ef9739b222272464f55371f1915d99920faed
Marouf <maroufibrahimi7264@gmail.com>
2023-02-26 19:07:51 -0800

    added ContactList

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 import AddContact from "./AddContact/AddContact";
+import ContactList from "./ContactList/ContactList";
+
+import './App.css'
 
 const App = () => {
   return (
     <div className="container">
       <div>
         <AddContact />
+      </div>
+
+      <div>
+        <ContactList />
       </div>
     </div>
   );
